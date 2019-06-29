d134da88e4305b9935592b02bcdfb68bdee34c6f
Sergei Morozov <morozserge1st@gmail.com>
2019-06-29 11:31:30 +0300

    Added two cards matching

--- script.js
+++ script.js
@@ -20,7 +20,27 @@ function flipCard(){
    	hasFlippedCard = false;
    	secondCard = this;
    }
+
+   if (firstCard.dataset.emoji === secondCard.dataset.emoji) {
+   	disableCard;
+   }
+   else {
+   	setTimeout(function(){
+   		firstCard.classList.remove('is-flipped');
+   		secondCard.classList.remove('is-flipped');
+   	}, 1500);
+
+   }
 };
+
+function disableCard() {
+	firstCard.removeEventListener("click", flipCard);
+   	secondCard.removeEventListener("click", flipCard);
+}
+
+function removeCard() {
+	
+}
 
 // Перемешивание карт, меняем порядок с помощью order
 (function mixingCards () {
